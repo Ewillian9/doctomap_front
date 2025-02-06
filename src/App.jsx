6a0ae906175680615f4ad1e27cf0e2e8010d0ac9
Ewillian9 <janonguittard@gmail.com>
2025-02-06 15:02:08 +0100

    import components

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,23 @@
 import { useState } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Header from './Components/Header/Header';
 import Doctors from './Pages/Doctors/Doctors';
 import DoctorDetail from './Pages/DoctorDetail/DoctorDetail';
 import Error from './Pages/Error/Error';
+import Footer from './Components/Footer/Footer';
 import './App.css';
 
 function App() {
 
   return (
     <BrowserRouter>
-      {/* <Header /> */}
+      <Header />
       <Routes>
         <Route path="/doctors" element={<Doctors />} />
         <Route path="/doctor/:id" element={<DoctorDetail />} />
         <Route path="*" element={<Error />} />
       </Routes>
-      {/* <Footer /> */}
+      <Footer />
     </BrowserRouter>
   )
 }
