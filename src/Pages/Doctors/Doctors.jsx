b75ae874861492bec6f00f108ecb58cce0fb3d27
Ewillian9 <janonguittard@gmail.com>
2025-02-06 13:49:54 +0100

    Doctors list

--- src/Pages/Doctors/Doctors.jsx
+++ src/Pages/Doctors/Doctors.jsx
@@ -1,10 +1,38 @@
-
+import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 
 const Doctors = () => {
-    
+    const [doctors, setDoctors] = useState([]);
+
+    useEffect(() => {
+        getDoctors();
+    }, [])
+
+    const getDoctors = async () => {
+        try {
+            const response = await fetch("https://127.0.0.1:8000/api/doctors");
+
+            const data = await response.json();
+            setDoctors(data.member);
+        } catch (error) {
+            console.error(error)
+        }
+    }
 
     return (
-        <h1>Doctors</h1>
+        <div className="doctors">
+            <div>
+                {doctors.map((doctor) => (
+                    <Link to={`/doctor/${doctor.id}-${doctor.firstname}-${doctor.lastname}`} key={doctor.id}>
+                        <img src={doctor.image} alt={doctor.firstname} />
+                        <div>
+                            <h2>{doctor.firstname} {doctor.lastname}</h2>
+                            <h2>{doctor.phone}</h2>
+                        </div>
+                    </Link>
+                ))}
+            </div>
+        </div>
         
     );
 };
