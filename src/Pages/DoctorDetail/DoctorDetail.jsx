8d31c26d2975a492fe4a91e486f08bbf7b37e942
Ewillian9 <janonguittard@gmail.com>
2025-02-06 13:50:04 +0100

    basic doctor detail

--- src/Pages/DoctorDetail/DoctorDetail.jsx
+++ src/Pages/DoctorDetail/DoctorDetail.jsx
@@ -1,10 +1,36 @@
-
+import { useState, useEffect } from 'react';
+import { useParams, Link } from 'react-router-dom';
 
 const DoctorDetail = () => {
-    
+    const [detail, setDetail] = useState([]);
+    const { id } = useParams();
 
+    useEffect(() => {
+        getDetail();
+    }, [])
+
+    const getDetail = async () => {
+        try {
+            const response = await fetch(`https://127.0.0.1:8000/api/doctors/${id}`);
+
+            const data = await response.json();
+            setDetail(data);
+        } catch (error) {
+            console.error(error)
+        }
+    }
     return (
-        <h1>DoctorDetail</h1>
+        <div className="doctors">
+            <div>
+                <img src={detail.image} alt={detail.firstname} />
+                <div>
+                    <h2>{detail.firstname} {detail.lastname}</h2>
+                    <h2>{detail.speciality}</h2>
+                    <h2>{detail.phone}</h2>
+                    <h2>{detail.address}, {detail.city}, {detail.zip}</h2>
+                </div>
+            </div>
+        </div>
         
     );
 };
